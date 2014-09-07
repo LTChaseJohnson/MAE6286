22a2273dd2883c76c2473daa5e9978ff27d36201
LTChaseJohnson <chasevjohnson@gmail.com>
2014-09-07 17:57:11 -0400

    Added np.arange

--- Lesson 2 Phugoid Oscillation.py
+++ Lesson 2 Phugoid Oscillation.py
@@ -4,4 +4,6 @@ import matplotlib.pyplot as plt
 T = 100.0                               #Max iteration value
 dt = 0.01                               #Step size
 N = np.int(T/dt)+1                      #Converts T/dt into an integer for linspace
-t = np.linspace(0.0,T,N)                #Creates an array from 0.0 to T
+t1 = np.linspace(0.0,T,N)               #Creates an array from 0.0 to T
+t = np.arange(0.0,T+dt,dt)              #Creates same array from 0.0 to T
+
