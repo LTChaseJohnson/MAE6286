1a93b071407db12f8635e5e60c97e5233bee8905
LTChaseJohnson <chasevjohnson@gmail.com>
2014-09-21 19:19:20 -0400

    Fixed abs(v)

--- Rocket Problem.py
+++ Rocket Problem.py
@@ -4,15 +4,15 @@ from math import *
 
 #Constants
 ms = 50         #Mass of rocket shell
-g = 9.8         #Gravitational acceleration
+g = 9.81         #Gravitational acceleration
 rho = 1.091     #Air density
 r = 0.5         #Cross sectional radius of rocket
-A = pi*r**2     #Cross sectional area
-ve = 325        #Exhaust speed
+A = np.pi*r**2     #Cross sectional area
+ve = 325.        #Exhaust speed
 Cd = 0.15       #Drag coefficient
-mp0 = 100       #Initial fuel mass
-h0 = 0          #Initial height
-v0 = 0          #Initial velocity
+mp0 = 100.       #Initial fuel mass
+h0 = 0.          #Initial height
+v0 = 0.          #Initial velocity
 
 T = 37.33
 dt = 0.1
@@ -29,7 +29,7 @@ u[0] = np.array([h0,v0])
 def f(u):
     h = u[0]
     v = u[1]
-    return np.array([v,-g+(mp_dot[n]*ve)/(ms+mp[n])-(rho*A*Cd*v**2)/(2*(ms+mp[n]))])
+    return np.array([v,-g+(mp_dot[n]*ve)/(ms+mp[n])-(rho*A*Cd*v*abs(v))/(2*(ms+mp[n]))])
 
 def euler_step(u, f, dt):
     return u + dt * f(u)
@@ -40,11 +40,23 @@ for n in range(N-1):
 
 h = u[:,0]
 v = u[:,1]
+tmaxh = np.where(v<=0.00001)[0][1]
+tcrash = np.where(h<=0)[0][2]
+
+maxv = max(v)
+maxh = max(h)
+
+print 'Fuel Remaining: ',mp[3.2]
+print 'Maximum Velocity: ',maxv
+print 'Height at max V: ',h[50]
+print 'Maximum Height: ',(maxh+1)/10.
+print 'Max Height time: ',(tmaxh+1)/10.
+print 'Time of Flight: ',tcrash
+print 'Crash Velocity: ',v[tcrash]
 
 
-print 'h = ',h
 
 plt.plot(t,h)
 plt.plot(t,v)
-plt.ylim(0.0,1500)
+plt.ylim(-90,1350)
 plt.show()
