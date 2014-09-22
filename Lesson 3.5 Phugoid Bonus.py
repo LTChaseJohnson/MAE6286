fea078a0bbb1cd3e304da8878b87b5157f9c0278
LTChaseJohnson <chasevjohnson@gmail.com>
2014-09-22 17:06:08 -0400

    Added Centered Difference Method

--- Lesson 3.5 Phugoid Bonus.py
+++ Lesson 3.5 Phugoid Bonus.py
@@ -38,6 +38,10 @@ def modeulerstep(u,f,dt):
     u_half = u + 0.5*dt*f(u)
     return u + dt*f(u_half)
 
+#Centered Difference Method
+def centereddiff(unm1,u,f,dt):
+    return unm1+2.0*dt*f(u)
+    
 #Defining time step parameters
 T = 15.0
 dt = 0.01
@@ -46,21 +50,31 @@ N = int(T/dt)+1
 #Creating empty solution vectors
 u_euler = np.empty((N,4))
 u_modeuler = np.empty((N,4))
+u_cd = np.empty((N,4))
 
 #Storing initial conditions
 u_euler[0] = np.array([v0,theta0,x0,y0])
 u_modeuler[0] = np.array([v0,theta0,x0,y0])
+u_cd[0] = np.array([v0,theta0,x0,y0])
+
+u_cd[1] = modeulerstep(u_cd[0],f,dt)     #First iteration with modified Euler method
 
 #Running the Loop!
 for n in range(N-1):
     u_euler[n+1] = eulerstep(u_euler[n],f,dt)
     u_modeuler[n+1] = modeulerstep(u_modeuler[n],f,dt)
 
+for n in range(1,N-1):
+    u_cd[n+1] = centereddiff(u_cd[n-1],u_cd[n],f,dt)
+
 x_euler = u_euler[:,2]
 y_euler = u_euler[:,3]
 
 x_modeuler = u_modeuler[:,2]
 y_modeuler = u_modeuler[:,3]
+
+x_cd = u_cd[:,2]
+y_cd = u_cd[:,3]
 
 crash_index = np.where(y_euler<=0)[0][0]
 
@@ -71,6 +85,7 @@ plt.xlabel('x $m$')
 plt.ylabel('Height $m$')
 plt.plot(x_euler,y_euler,'k-',label='Euler')
 plt.plot(x_modeuler,y_modeuler,'r--',label='Modified Euler')
+plt.plot(x_cd,y_cd,'b-.',label='Centered Difference')
 plt.xlim(x0,x_euler[crash_index]+1)
 plt.legend(loc='best')
 plt.ylim(0,2.5)
@@ -111,5 +126,6 @@ plt.loglog(dt_values[:-1],diffgrid_mod_e[:-1], color='r', ls='--',marker='o',lab
 plt.xlim(1e-4,1)
 plt.ylim(1e-4,10)
 plt.legend(loc='best')
+plt.axis('equal')
 
 plt.show()
