864b2ab78067e4913777aceb3a497cebb4458654
LTChaseJohnson <chasevjohnson@gmail.com>
2014-09-21 20:16:33 -0400

    Cleaned up comments and added ground line in graph

--- Rocket Problem.py
+++ Rocket Problem.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 from math import *
 
 #Constants
-ms = 50         #Mass of rocket shell
-g = 9.81         #Gravitational acceleration
-rho = 1.091     #Air density
-r = 0.5         #Cross sectional radius of rocket
-A = np.pi*r**2     #Cross sectional area
-ve = 325.        #Exhaust speed
-Cd = 0.15       #Drag coefficient
-mp0 = 100.       #Initial fuel mass
-h0 = 0.          #Initial height
-v0 = 0.          #Initial velocity
+ms = 50           #Mass of rocket shell
+g = 9.81          #Gravitational acceleration
+rho = 1.091       #Air density
+r = 0.5           #Cross sectional radius of rocket
+A = np.pi*r**2    #Cross sectional area
+ve = 325.         #Exhaust speed
+Cd = 0.15         #Drag coefficient
+mp0 = 100.        #Initial fuel mass
+h0 = 0.           #Initial height
+v0 = 0.           #Initial velocity
 
 T = 50.
 dt = 0.1
 N = int(T/dt)+1
 t = np.linspace(0.0,T,N)
 
-mp_dot = np.piecewise(t,[t<5,t>=5],[20,0])  #Defines piecewise function mp_dot
-mp = np.piecewise(t,[t<5,t>=5],[lambda t: mp0-20*t, lambda t: 0])
+mp_dot = np.piecewise(t,[t<5,t>=5],[20,0])                           #Defines piecewise function mp_dot
+mp = np.piecewise(t,[t<5,t>=5],[lambda t: mp0-20*t, lambda t: 0])  #Defines piecewise function mp
 
 
-u = np.empty((N,2))
-u[0] = np.array([h0,v0])
+u = np.empty((N,2))            #Creates empty array to store solutions
+u[0] = np.array([h0,v0])       #Puts initial conditions into array
 
 def f(u):
     h = u[0]
@@ -59,7 +59,9 @@ print 'Crash Velocity: ',v[tcrash]
 
 plt.plot(t,h)
 plt.plot(t,v)
+plt.axhline(y=0,linewidth=3,color='k')
 plt.ylim(-90,1350)
+plt.xlim(0.0,t[tcrash+.75])
 plt.title('Rocket Position and Velocity')
 plt.ylabel('Rocket Velocity $m/s$, Rocket Position $m$')
 plt.xlabel('Time $s$')
