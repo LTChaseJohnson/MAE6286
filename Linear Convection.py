fafec152abeddc526c5867ef5dbc908416eae5bb
LTChaseJohnson <chasevjohnson@gmail.com>
2014-09-22 20:38:11 -0400

    Final Push

--- Linear Convection.py
+++ Linear Convection.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 from math import *
 
 #Initial Conditions
-nx = 81
-nt = 30
-c = 1
+nx = 40
+nt = 10
+c = 1.
 x = np.linspace(0.0,2.,nx)
 u = np.piecewise(x,[x<0.5,x>=0.5,x>=1.0],[1.0,2.0,1.0])
 u0 = np.piecewise(x,[x<0.5,x>=0.5,x>=1.0],[1.0,2.0,1.0])
-dt = 0.02
+dt = 0.01
 dx = 2./(nx-1)
-
+print dt/dx
 for n in range(1,nt):
     un = u.copy()
     for i in range(1,nx):
