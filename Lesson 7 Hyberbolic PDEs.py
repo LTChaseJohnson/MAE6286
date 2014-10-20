99153a63134273b9b492b59dfcf09f907aff3dc9
LTChaseJohnson <chasevjohnson@gmail.com>
2014-10-20 17:10:57 -0400

    Having problems with Maccormick

--- Lesson 7 Hyberbolic PDEs.py
+++ Lesson 7 Hyberbolic PDEs.py
@@ -26,4 +26,70 @@ plt.plot(x,rho)
 plt.ylabel('Traffic Density')
 plt.xlabel('Distance')
 plt.ylim(-.5,11)
+
+def F(V_max, rho_max, rho):
+    return V_max*rho*(1-rho/rho_max)
+
+def animate(data):
+    x = np.linspace(0,4,nx)
+    y = data
+    line.set_data(x,y)
+    return line,
+
+def LaxFred(rho, nt, dt, nx, rho_max, V_max):
+    rhon = np.zeros((nt,len(rho)))
+    rhon[:,:] = rho.copy()
+    
+    for t in range(1,nt):
+        Flux = F(V_max, rho_max, rho)
+        rhon[t,1:-1] = 0.5*(rho[2:]+rho[:-2])-dt/(2*dx)*(Flux[2:]-Flux[:-2])
+        rhon[t,0] = rho[0]
+        rhon[t,-1] = rho[-1]
+        rho = rhon[t].copy()
+    
+    return rhon
+
+sigma = 1.
+dt = sigma*dx
+
+rho = rho_red_light(nx, rho_max, rho_in)
+rhon = LaxFred(rho, nt, dt, nx, rho_max, V_max)
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0,4),ylim=(-.5,11),xlabel=('Distance'),ylabel=('Traffic Density'))
+line, = ax.plot([],[],color='r',lw=2)
+
+anim = animation.FuncAnimation(fig, animate, frames=rhon, interval=50)
+display_animation(anim, default_mode='once')
+
+def Maccormack(rho, nt, dt, dx, V_max, rho_max):
+    rhon = np.zeros((nt,len(rho)))
+    rho_star = np.empty_like(rho)
+    rhon[:,:] = rho.copy()
+    rho_star[:,:] = rho.copy()
+    
+    for t in range(1,nt):
+        Flux = F(V_max, rho_max, rho)
+        rho_star[:-1] = rho[:-1]-dt/dx*(Flux[1:]-Flux[:-1])
+        Flux_star = F(V_max, rho_max, rho_star)
+        rhon[t,1:] = 0.5*(rho[1:]+rho_star[1:]-dt/dx*(Flux_star[1:]-Flux_star[:-1]))
+        
+        rho = rhon[t].copy()
+        
+        return rhon
+
+rho = rho_red_light(nx, rho_max, rho_in)
+sigma = 1.0
+dt = sigma*dx
+
+rhon = Maccormack(rho, nt, dt, dx, V_max, rho_max)
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0,4),ylim=(-.5,11),xlabel=('Distance'),ylabel=('Traffic Density'))
+line, = ax.plot([],[],color='r',lw=2)
+
+anim = animation.FuncAnimation(fig, animate, frames=rhon, interval=50)
+display_animation(anim, default_mode='reflect')
+        
+
 plt.show()
