d2e082f3161a54d8a805d9ef3c4bcde7e3b53fd3
LTChaseJohnson <chasevjohnson@gmail.com>
2014-11-02 09:16:47 -0500

    Added minimum density

--- A Better Model.py
+++ A Better Model.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy
 
-u_max,u_star,rho_max,rho_star,A,B = sympy.symbols('u_max u_* rho_max rho_* A B')
+u_max,u_star,rho_max,rho_star,A,B,u_wave,rho_min = sympy.symbols('u_max u_* rho_max rho_* A B u_wave rho_min')
 
 eq1 = sympy.Eq(0,u_max*rho_max*(1-A*rho_max-B*rho_max**2))
 eq2 = sympy.Eq(0,u_max*(1-2*A*rho_star-3*B*rho_star**2))
@@ -16,8 +16,14 @@ B_sol = sympy.solve(eq1,B)[0]
 quad_A = eq2.subs([(rho_star,rho_starsolve),(B,B_sol)])
 A_sol = sympy.solve(quad_A,A)
 
-A1 = A_sol[0].evalf(subs={u_star:0.7,u_max:1.0,rho_max:10.})
-A2 = A_sol[1].evalf(subs={u_star:0.7,u_max:1.0,rho_max:10.})
-B = B_sol.evalf(subs={A: A2,rho_max:10.})
+A1 = A_sol[0].evalf(subs={u_star:1.5,u_max:2.0,rho_max:15.})
+A2 = A_sol[1].evalf(subs={u_star:1.5,u_max:2.0,rho_max:15.})
+B = B_sol.evalf(subs={A: A2,rho_max:15.})
 
-print A1, A2, B
+eq5 = sympy.Eq(0,u_max*(1-2*A*rho_min-3*B*rho_min**2))
+rho_min_sol = sympy.solve(eq5,rho_min)
+
+print 'A1 = ',A1
+print 'A2 = ',A2
+print 'B = ',B
+print 'rho_min = ',rho_min_sol
