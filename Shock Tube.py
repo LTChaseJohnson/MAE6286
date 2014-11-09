351a6e3d925c304f9c0f76001cc7a8e5f8a079f5
LTChaseJohnson <chasevjohnson@gmail.com>
2014-11-09 13:09:41 -0500

    Added MUSCL to Shock Tube

--- Shock Tube.py
+++ Shock Tube.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-def f(u):
-    return np.array([u[1],u[1]**2/u[0]+(gamma-1)*(u[2]-0.5*u[1]**2/u[0]),
-    (u[2]+(gamma-1)*(u[2]-0.5*u[1]**2/u[0]))*u[1]/u[0]])
+from matplotlib import animation
+from JSAnimation.IPython_display import display_animation
 
 T = 0.01                      #Final Time
 dt = 0.0002
@@ -47,13 +45,18 @@ plt.plot(x,uINIT[2],label='U3')
 plt.legend(loc='best')
 
 #Creating Space-Time-U Grid
-ust = np.empty((3,nx,nt))
-ust[:,:,0] = uINIT[:,:]
-
+usti = np.empty((3,nx,nt))
+usti[:,:,0] = uINIT[:,:]
 
 def f(u):
     f = np.array([u[1],u[1]**2/u[0]+(gamma-1)*(u[2]-.5*u[1]**2/u[0]),(u[2]+(gamma-1)*(u[2]-.5*u[1]**2/u[0]))*u[1]/u[0]])
     return f
+
+def animate(data):
+    x = np.linspace(-10,10,nx)
+    y = data
+    line.set_data(x,y)
+    return line,
 
 def Richtmyer(ust,nx,nt,dt,dx):
     un = ust[:,:,0]    
@@ -69,11 +72,19 @@ def Richtmyer(ust,nx,nt,dt,dx):
         
     return ust
 
-u = Richtmyer(ust,nx,nt,dt,dx)
+ust = Richtmyer(usti,nx,nt,dt,dx)
 
-U125=u[0,50,-1]
-U225=u[1,50,-1]
-U325=u[2,50,-1]
+velocity = ust[1,:,:]/ust[0,:,:]
+density = ust[0,:,:]
+pressure = (gamma-1)*(ust[2,:,:]-0.5*(ust[1,:,:]**2/ust[0,:,:]))
+
+pltvelocity = np.swapaxes(velocity,0,1)
+pltdensity = np.swapaxes(density,0,1)
+pltpressure = np.swapaxes(pressure,0,1)
+
+U125=ust[0,50,-1]
+U225=ust[1,50,-1]
+U325=ust[2,50,-1]
 
 print ('U1 at 2.5m: '),U125
 print ('U2 at 2.5m: '),U225
@@ -87,4 +98,89 @@ print ('Velocity at 2.5m: '),u25
 print ('Pressure at 2.5m: '),P25
 print ('Density at 2.5m: '),rho25
 
-plt.show()
+fig = plt.figure();
+ax = plt.axes(xlim=(-10,10),ylim=(0,600),xlabel=('Distance'),ylabel=('Velocity'));
+line, = ax.plot([],[],color='k',lw=2);
+
+anim =  animation.FuncAnimation(fig,animate,frames=pltvelocity[1:,:],interval=50)
+display_animation(anim,default_mode='reflect')
+
+fig = plt.figure();
+ax = plt.axes(xlim=(-10,10),ylim=(0,1.1),xlabel=('Distance'),ylabel=('Density'));
+line, = ax.plot([],[],color='k',lw=2);
+
+anim =  animation.FuncAnimation(fig,animate,frames=pltdensity[1:,:],interval=50)
+display_animation(anim,default_mode='reflect')
+
+fig = plt.figure();
+ax = plt.axes(xlim=(-10,10),ylim=(9900,101000),xlabel=('Distance'),ylabel=('Pressure'));
+line, = ax.plot([],[],color='k',lw=2);
+
+anim =  animation.FuncAnimation(fig,animate,frames=pltpressure[1:,:],interval=50)
+display_animation(anim,default_mode='reflect')
+
+def MUSCL(ust,nx,nt,dt,dx):
+    un = ust[:,:,0]
+    uni = ust[:,:,0]
+    u_plus = np.zeros((3,nx))
+    u_minus = np.zeros((3,nx))
+    flux = np.zeros((3,nx))
+    u_star = np.zeros((3,nx))
+    
+    for i in range(1,nt):
+# * Step    
+        u_plus[:,:-1] = un[:,1:]
+        u_minus[:,:] = un[:,:]
+    
+        flux = 0.5*(f(u_minus)+f(u_plus)
+                -dx/dt*(u_plus-u_minus))
+        
+        u_star[:,1:-1] = un[:,1:-1]+dt/dx*(flux[:,:-2]-
+                                           flux[:,1:-1])
+        u_star[:,0] = un[:,0]
+        u_star[:,-1]  = un[:,-1]
+# n+1 Step using * values
+        u_plus[:,:-1] = u_star[:,1:]
+        u_minus[:,:] = u_star[:,:]
+    
+        flux = 0.5*(f(u_minus)+f(u_plus)
+                -dx/dt*(u_plus-u_minus))
+    
+        un[:,1:-1] = 0.5*(un[:,1:-1]+u_star[:,1:-1]+
+                         dt/dx*(flux[:,:-2]-flux[:,1:-1]))
+        
+        ust[:,:,i] = un[:,:]
+        ust[:,:,0] = uni[:,:]
+    
+    return ust  
+
+ust = MUSCL(usti,nx,nt,dt,dx)
+
+velocity = ust[1,:,:]/ust[0,:,:]
+density = ust[0,:,:]
+pressure = (gamma-1)*(ust[2,:,:]-0.5*(ust[1,:,:]**2/ust[0,:,:]))
+
+pltvelocity = np.swapaxes(velocity,0,1)
+pltdensity = np.swapaxes(density,0,1)
+pltpressure = np.swapaxes(pressure,0,1)
+
+fig = plt.figure();
+ax = plt.axes(xlim=(-10,10),ylim=(0,600),xlabel=('Distance'),ylabel=('Velocity'));
+line, = ax.plot([],[],color='k',lw=2);
+
+anim =  animation.FuncAnimation(fig,animate,frames=pltvelocity[1:,:],interval=50)
+display_animation(anim,default_mode='reflect')
+
+fig = plt.figure();
+ax = plt.axes(xlim=(-10,10),ylim=(0,1.1),xlabel=('Distance'),ylabel=('Density'));
+line, = ax.plot([],[],color='k',lw=2);
+
+anim =  animation.FuncAnimation(fig,animate,frames=pltdensity[1:,:],interval=50)
+display_animation(anim,default_mode='reflect')
+
+fig = plt.figure();
+ax = plt.axes(xlim=(-10,10),ylim=(9900,101000),xlabel=('Distance'),ylabel=('Pressure'));
+line, = ax.plot([],[],color='k',lw=2);
+
+anim =  animation.FuncAnimation(fig,animate,frames=pltpressure[1:,:],interval=50)
+display_animation(anim,default_mode='reflect')
