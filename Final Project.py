b7c3c5cc492a647b725d6f338da731b7a4ad0a95
LTChaseJohnson <chasevjohnson@gmail.com>
2014-11-29 08:11:42 -0500

    Fixe Initial Condition Bug

--- Final Project.py
+++ Final Project.py
@@ -14,12 +14,12 @@ nx = 100
 x = np.linspace(0,L,nx)
 dx = L/(nx-1)
 nt = 500
-c = 5.0
+c = 500.0
 dt = 0.5*dx/c
 
 g = lambda x: -2+2*np.cos(2*np.pi/L*x)
 veli = g(x)
-yi = u = np.piecewise(x, [x<L/2,x>=L/2],[lambda x: 2*u_max/L*x,lambda x: 2*u_max*(-x/L+1)])
+yi = u_initial(u_max,L)
 lam = c*dt/dx
 print lam**2
 
@@ -33,13 +33,18 @@ def ctcs(nt,dt,ui,lam):
     yn = np.zeros((nt,len(yi)))
     yn[0,:] = ui
     y = yn.copy()
-    y[0,1:-1] = yn[0,1:-1]-dt*veli[1:-1]
-    y[1,1:-1] = 2*y[0,1:-1]*(1-lam**2)+lam**2*(y[0,2:]+y[0,:-2])-y[0,1:-1]+dt*veli[1:-1]
-    for i in range(nt-1):
+    y[0,1:-1] = yn[0,1:-1]
+    y[1,1:-1] = y[0,1:-1]+dt*veli[1:-1] 
+    for i in range(1,nt-1):
         y[i+1,1:-1] = 2*y[i,1:-1]*(1-lam**2)+lam**2*(y[i,2:]+y[i,:-2])-y[i-1,1:-1]
     return y
 
 y = ctcs(nt,dt,yi,lam)
+
+plt.figure(1)
+plt.plot(x,y[1,:],label='Time 1')
+plt.plot(x,y[2,:],label='Time 2')
+plt.legend()
 
 for i in range(nt):
     plt.figure(2)
@@ -48,3 +53,20 @@ for i in range(nt):
     plt.xlim(0,L)
     
 plt.show()
+
+'''
+from matplotlib import animation
+from JSAnimation.IPython_display import display_animation
+
+def animate(data):
+    x = np.linspace(0,L,nx)
+    y = data
+    line.set_data(x,y)
+    return line,
+
+fig = plt.figure();
+ax = plt.axes(xlim=(0,L),ylim=(-u_max,u_max))
+line, = ax.plot([],[],lw=2);
+
+anim = animation.FuncAnimation(fig, animate, frames = y, interval = 50)
+display_animation(anim,default_mode='reflect')'''
